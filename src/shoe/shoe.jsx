44ee4404d7361f1c70501387d266be325159bfcd
kyhaoboot <kyhaoboot@gmail.com>
2025-07-17 21:27:44 +0700

    Finish Project

--- src/shoe/shoe.jsx
+++ src/shoe/shoe.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import { useState } from "react";
 import ShoeDetail from "./shoeDetail";
 
 export default function Shoe(props) {
   const { product } = props;
-  // const [Detail, setDetail] = useState([]);
+  const [showDetail, setShowDetail] = useState(false);
+  // console.log(props);
+  const handleShowDetail = () => {
+    setShowDetail(true);
+  };
+  const handleCloseDetail = () => {
+    setShowDetail(false);
+  };
   return (
     <div>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
@@ -21,17 +28,21 @@ export default function Shoe(props) {
           </p>
           {/* Modal toggle */}
 
-          <ShoeDetail />
+          {/* <ShoeDetail /> */}
           {/* <ShoeDetail /> */}
 
           <button
+            onClick={handleShowDetail}
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
-            Mua
+            Chi Tiết
           </button>
         </div>
       </div>
+      {showDetail && (
+        <ShoeDetail product={product} onClose={handleCloseDetail} />
+      )}
     </div>
 
     /////////////////////////////////
